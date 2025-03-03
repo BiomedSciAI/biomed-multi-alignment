40cb8b74cb47a6d7b1c71e70910ceaab3648b256
MATAN NINIO <MATANN@il.ibm.com>
2025-03-03 15:31:01 +0200

    removed commented out sections

--- mammal/examples/scrna_cell_type/scRNA_infer.py
+++ mammal/examples/scrna_cell_type/scRNA_infer.py
@@ -69,25 +69,11 @@ def main(
     )
     data_source.create()
 
-    # module(
-    #     task=task,
-    #     model=model,
-    #     **OmegaConf.to_container(cfg.module, resolve=True),
-    # )
-
     sample_dict = create_sample_dict(task_name, data_source, sample_id, tokenizer_op)
 
-    # result = task_infer(task_dict=task_dict, smiles_seq=sample_id)
     batch_dict = get_predictions(nn_model, sample_dict)
-    # running in generate mode
-
-    # batch_dict = process_sample(
-    #     tokenizer_op, nn_model, sample_dict
-    # )
 
     ans = process_model_output(tokenizer_op, batch_dict)
-    # print(f"The prediction for {sample_id=} is {result}")
-    # print_result(batch_dict, scalars_preds_processed_key)
     ans = {
         k: v.detach().numpy() if isinstance(v, torch.Tensor) else v
         for k, v in ans.items()
@@ -101,76 +87,6 @@ def process_model_output(tokenizer_op, batch_dict):
         decoder_output=batch_dict[CLS_PRED][0],
         decoder_output_scores=batch_dict[SCORES][0],
     )
-
-
-# def load_model(task_name: str, device: str) -> dict:
-
-#     # path = "ibm/biomed.omics.bl.sm.ma-ted-458m"
-#     path = "/dccstor/mm_hcls/usr/matanin/mammal_extention/scrna_cell_type/mammalian_scRNA_cell_type_zeng68_24h.ckpt"
-
-#     # Load Model and set to evaluation mode
-#     model = Mammal.from_pretrained(path)
-#     model.eval()
-#     model.to(device=device)
-
-#     # Load Tokenizer
-#     tokenizer_op = ModularTokenizerOp.from_pretrained(path)
-
-#     task_dict = dict(
-#         task_name=task_name,
-#         model=model,
-#         tokenizer_op=tokenizer_op,
-#     )
-#     return task_dict
-
-
-# def process_model_output(
-#     tokenizer_op: ModularTokenizerOp,
-#     decoder_output: np.ndarray,
-#     decoder_output_scores: np.ndarray,
-# ) -> dict | None:
-#     """
-#     Extract predicted class and scores
-#     """
-#     return CellTypeTask.process_model_output(
-#         tokenizer_op,
-#         decoder_output=decoder_output,
-#         decoder_output_scores=decoder_output_scores,
-#     )
-#     # negative_token_id = tokenizer_op.get_token_id("<0>")
-#     # positive_token_id = tokenizer_op.get_token_id("<1>")
-#     # label_id_to_int = {
-#     #     negative_token_id: 0,
-#     #     positive_token_id: 1,
-#     # }
-#     # classification_position = 1
-
-#     # if decoder_output_scores is not None:
-#     #     scores = decoder_output_scores[classification_position, positive_token_id]
-
-#     # ans = dict(
-#     #     pred=label_id_to_int.get(int(decoder_output[classification_position]), -1),
-#     #     score=scores.item(),
-#     # )
-#     # return ans
-
-
-# def task_infer(task_dict: dict, smiles_seq: str) -> dict | None:
-#     task_name = task_dict["task_name"]
-#     model = task_dict["model"]
-#     tokenizer_op = task_dict["tokenizer_op"]
-
-#     sample_dict = create_sample_dict(task_name, smiles_seq, tokenizer_op)
-#     # Generate Prediction
-#     batch_dict = get_predictions(model, sample_dict)
-
-#     # Post-process the model's output
-#     result = process_model_output(
-#         tokenizer_op=tokenizer_op,
-#         decoder_output=batch_dict[CLS_PRED][0],
-#         decoder_output_scores=batch_dict[SCORES][0],
-#     )
-#     return result
 
 
 def get_tokenizer_and_model(tokenizer_path, model_path, device):
@@ -192,10 +108,6 @@ def create_sample_dict(
     sequence_key = "scrna.scrna"
     # Create and load sample
     sample_dict = data_source[sample_id]
-    # {
-    #     sequence_key: data_source[sample_id],
-    #     SAMPLE_ID: sample_id,
-    # }
 
     sample_dict = CellTypeTask.data_preprocessing(
         sample_dict,
@@ -228,9 +140,6 @@ def print_result(batch_dict, scalars_preds_processed_key):
 
     batch_dict["scalar_result"] = value
     print(f"estimated value: {ans}")
-    # for k, v in batch_dict.items():
-    #     if "model" in k:
-    #         print(f"{k}: {v}")
 
 
 def process_sample(tokenizer_op, nn_model, sample_dict):
